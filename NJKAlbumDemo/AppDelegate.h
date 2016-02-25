10d6bb4c270fde9d5fee4a8d7ca49e21eec3ece6
njk1991 <njk0207@icloud.com>
2016-02-25 15:20:07 +0800

    Initial Commit

---
+++ NJKAlbumDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NJKAlbumDemo
+//
+//  Created by JiakaiNong on 16/2/25.
+//  Copyright © 2016年 poco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
